c283d16362920b0868325576c216e98a7d671d0e
morisum <moris@r.sannet.ne.jp>
2024-08-17 09:02:09 +0900

    Initial Commit

---
+++ MyTimerTest2/MyTimerTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerTest2App.swift
+//  MyTimerTest2
+//
+//  Created by MsMacM on 2024/08/17.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
